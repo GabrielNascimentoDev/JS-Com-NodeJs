acd4b52195e59e733d19ebc3b392ffd400ca9a98
GabrielNascimentoDev <biel.henrique.s.n@gmail.com>
2024-08-14 11:10:54 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
-    console.log(texto);
+     verificaPalavrasDuplicadas(texto);
 })
 
 console.log(link);
+
+function verificaPalavrasDuplicadas(texto){
+    const listaPalavras = texto.split(' ')
+    const resultado = {}
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0 ) + 1
+    }); 
+    
+}
