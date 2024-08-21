559a1113374dd89bc89d62110343ca927c33f8f3
GabrielNascimentoDev <biel.henrique.s.n@gmail.com>
2024-08-21 11:28:27 -0300

    indentificando erros

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
-    console.log('qual é o erro?', erro);
-    
+    if (erro){
+        console.log('qual é o erro?', erro);
+    }
     contaPalavras(texto);
 })
 
