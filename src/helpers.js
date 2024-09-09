2159009ae1c0a4753994c70ead2ad51849168c50
GabrielNascimentoDev <biel.henrique.s.n@gmail.com>
2024-09-09 11:21:11 -0300

    arrumando o cod

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 function filtraOcorrencias(paragrafo){
-  return Object.keys(paragrafo).filter(chave => paragrafo[chave] > 1)
+  return Object.keys(paragrafo).filter(chave => paragrafo[chave] > 1);
 }
 
 function montaSaidaArquivo(listaPalavras){
