dbb4f60d52e2e1397b84e044c6672559e17c6ff2
GabrielNascimentoDev <biel.henrique.s.n@gmail.com>
2024-09-09 11:03:33 -0300

    criando funcao filtraocorrencia

---
+++ src/helpers.js
@@ -0,0 +1,10 @@
+function filtraOcorrencias(paragrafo){
+  return Object.keys(paragrafo).filter(chave => paragrafo[chave] > 1)
+}
+
+function montaSaidaArquivo(listaPalavras){
+    let textoFinal = ('')
+    listaPalavras.forEach((paragrafo, indice) => {
+        const duplicadas = filtraOcorrencias(paragrafo).join(', ')
+    })
+}
